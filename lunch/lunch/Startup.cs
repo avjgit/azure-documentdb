326487dce57ba3db089d2cbb7b555ad628a99b98
AJ <avjmail@gmail.com>
2016-11-13 18:57:25 +0200

    Create entry from JSON

--- lunch/lunch/Startup.cs
+++ lunch/lunch/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using lunch.Models;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Owin;
+using Newtonsoft.Json;
 using Owin;
+using System.IO;
 
 [assembly: OwinStartupAttribute(typeof(lunch.Startup))]
 namespace lunch
@@ -9,6 +13,17 @@ namespace lunch
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            LoadData();
+        }
+
+        public async void LoadData()
+        {
+            //DocumentDB supports strongly typed POCO objects and also dynamic objects
+            Restaurant testRestaurant = JsonConvert.DeserializeObject<Restaurant>(File.ReadAllText("./App_Data/test.json"));
+            //dynamic dynamicObject = JsonConvert.DeserializeObject(File.ReadAllText(@".\App_Data\test2.json"));
+
+            //persist the documents in DocumentDB
+            await DocumentDBRepository<Restaurant>.CreateItemAsync(testRestaurant);
         }
     }
 }
